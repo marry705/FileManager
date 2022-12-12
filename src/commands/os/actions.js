9e9f63c7f518c0febb4ba11839b3942507e32bd9
marina.shilo <marina.shilo@degree53.com>
2022-12-12 23:12:22 +0300

    refactor: os actions

--- src/commands/os/actions.js
+++ src/commands/os/actions.js
@@ -1,9 +1,19 @@
 import { EOL, cpus, homedir, userInfo, arch } from 'os';
 import { OS_COMMANDS } from '../../helpers/index.js';
 
+const getCpus = () => (
+    [
+        `Amount is: ${cpus().length}`, 
+        cpus().map(({ model, speed }) => ({
+            model: model,
+            speed: speed/1000
+        }))
+    ]
+);
+
 export const actionsByCommand = {
     [OS_COMMANDS.EOL]: JSON.stringify(EOL),
-    [OS_COMMANDS.cpus]: cpus(),
+    [OS_COMMANDS.cpus]: getCpus(),
     [OS_COMMANDS.homedir]: homedir(),
     [OS_COMMANDS.username]: userInfo().username,
     [OS_COMMANDS.architecture]: arch(),
