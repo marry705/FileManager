b72afd7a9c970545de732672575f2cc63c8f46a7
marina.shilo <marina.shilo@degree53.com>
2022-12-18 23:40:11 +0300

    refactor: commands function

--- src/helpers/commands.js
+++ src/helpers/commands.js
@@ -1,6 +1,29 @@
 export const getCommandsAndArguments = (answer) => {
-    return answer
-            .trim()
-            .replace(/ +/g, ' ')
-            .split(' ');
+    const answerString = answer?.trim();
+
+    let command = '';
+    let argument = '';
+    let sec_argument = '';
+
+    command = answerString.indexOf(' ') !== -1 
+        ? answerString.slice(0, answerString.indexOf(' '))
+        : answerString 
+        || undefined;
+
+    const argumentsString = answerString.replace(command, ' ').trim();
+
+    if (argumentsString.startsWith(`"`)) {
+        argument = argumentsString.indexOf(`"`, 1) !== -1 
+            ? argumentsString.slice(0, argumentsString.indexOf(`"`, 1) + 1)
+            : argumentsString;
+    } else {
+        argument = argumentsString.indexOf(' ') !== -1 
+            ? argumentsString.slice(0, argumentsString.indexOf(' '))
+            : argumentsString
+            || undefined;
+    };
+
+    sec_argument = argumentsString.replace(argument, ' ').trim() || undefined;
+
+    return [ command, argument, sec_argument ];
 };
